d5cec030490eb23d4e79fc203016bb2954702bf0
foysalious <foysalrahman112@gmail.com>
2022-06-25 13:32:16 +0600

    default dsn added

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,7 +6,7 @@ export default registerAs(
   'typeOrmConfig',
   (): TypeOrmModuleOptions => ({
     type: 'mongodb',
-    url: process.env.MONGO_DSN,
+    url: process.env.MONGO_DSN??'mongodb://localhost:27017/restaurant',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
   }),
 );
